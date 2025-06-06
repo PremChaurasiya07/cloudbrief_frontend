c43bb3435562be81d5bbc61d9293742708ec870d
PremChaurasiya07 <premchaurasiya2006@gmail.com>
2025-06-06 22:04:19 +0530

    c

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,6 +3,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://imiviisdmfuedejubuqp.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltaXZpaXNkbWZ1ZWRlanVidXFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5MTA4MzEsImV4cCI6MjA1OTQ4NjgzMX0.yUTP_LLD85BpToRKH-6G9bSsLT9F_KwxizKw6UWueTg";
+const supabaseKey = `${import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY}`;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
